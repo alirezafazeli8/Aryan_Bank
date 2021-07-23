c3c503a04f53b32494d9f37163c8da53dcff5bc5
alirezafazeli8 <alirezafazeli34@gmail.com>
2021-07-23 14:02:49 +0430

    00/5/1 --refactore code--

--- js/app.js
+++ js/app.js
@@ -11,6 +11,102 @@ const submitButton = document.getElementById("submit-btn");
 // sweet alert
 const sweetIncorrectCard = (message) => swal({ title: message, icon: "error" });
 
+// information
+let targetUserSave = [];
+
+// check card functions (begin, target) =>  and if cards is incorrect function return to me undefined if is correct , return to me correct.
+function checkCard(whichCard) {
+  // get card information from API
+  for (let card of Object.values(cards)) {
+    // get correct string with string methods
+    const cardsFromDB = card.cardNumber.replaceAll("_", "");
+    if (whichCard.value == cardsFromDB) {
+      return true;
+    } else {
+      continue;
+    }
+  }
+}
+
+// NOTE thats my root of project is Important part of project (Process All Authentication);
+function allBeginInfo() {
+  // Check all Information of begin input card for money forwarding and error handling
+  function beginNumberCard() {
+    // EMPTY checker handler
+    if (beginInput.value == "") {
+      sweetIncorrectCard("Your Begin Card Is Empty");
+    } else {
+      // correct card check handler
+      if (checkCard(beginInput) == undefined) {
+        sweetIncorrectCard("Your Begin Card Is Not Correct");
+        return false;
+      } else {
+        // check all information should be true
+        for (let card of Object.values(cards)) {
+          // concat string
+          const cardsFromDB = card.cardNumber.replaceAll("_", "");
+          if (beginInput.value == cardsFromDB) {
+            // check money exist
+            if (moneyInput.value >= card.money) {
+              // check  cvv2
+              if (cvv2Input.value == card.cvv2) {
+                // check password
+                if (passwordInput.value == card.password) {
+                  return true;
+                } else {
+                  sweetIncorrectCard("Pleas Enter Correct Password");
+                  return false;
+                }
+              } else {
+                sweetIncorrectCard("Cvv2 Is Incorrect");
+                return false;
+              }
+            } else {
+              sweetIncorrectCard("You Don't Have Enough Money");
+              return false;
+            }
+          } else {
+            continue;
+          }
+        }
+      }
+    }
+  }
+
+  // target number card
+  // function targetNumberCard() {
+  //   // NOTE render incorrect number card
+  //   if (targetInput.value == "") {
+  //     sweetIncorrectCard("Your Target Card Is Empty");
+  //   } else {
+  //     if (checkCard(targetInput) == undefined) {
+  //       sweetIncorrectCard("Your Target Card Is Incorrect");
+  //     } else {
+  //       // checkCard(targetInput);
+  //     }
+  //   }
+  // }
+
+  function similarCard() {
+    if (beginInput.value != "" && targetInput.value != "") {
+      if (beginInput.value == targetInput.value) {
+        sweetIncorrectCard("Your Target Card is Equal To Begin");
+        return false;
+      } else {
+        return true;
+      }
+    }
+  }
+
+  if (!beginNumberCard() && !targetNumberCard() && !similarCard()) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+// -------------------------------------
+
 // target onblur event listener
 targetInput.onblur = function () {
   // show target person
